3eee3a93ecb27f29cd01339f110236a42e0ea5c2
Rendijs Smukulis <randy.smukulis@ericsson.com>
2017-10-15 22:50:47 +0300

    Added unit test coverage to the IntermediateDeserializer

--- netBencodeReader/netBencodeReaderUnitTests/IntermediateDeserializerTests.cs
+++ netBencodeReader/netBencodeReaderUnitTests/IntermediateDeserializerTests.cs
@@ -6,6 +6,7 @@
 
 using System.Diagnostics;
 using netBencodeReader.Tokenizer;
+using netBencodeReader.Types;
 
 namespace netBencodeReaderUnitTests
 {
@@ -20,19 +21,52 @@ namespace netBencodeReaderUnitTests
     public class IntermediateDeserializerTests
     {
         /// <summary>
-        /// Verifies GetBaseObject doesn't throw.
+        /// Verifies GetBaseObject parses numbers.
         /// </summary>
         [TestMethod]
-        public void IntermediateDeserializer_UnitTest_GoldenPath()
+        public void IntermediateDeserializer_UnitTest_Numbers()
         {
             var tokenizer = BencodeReader.Create(new StringReader("i24e"));
             var deser = new Deserializer();
             var num = deser.GetBaseObject(tokenizer);
+            
+            Assert.IsInstanceOfType(num, typeof(BENumber));
+            Assert.AreEqual(24, (num as BENumber).ToInt());
+            Assert.AreEqual(24L, (num as BENumber).ToLong());
+            Assert.AreEqual("24", num.ToString());
 
-            tokenizer = BencodeReader.Create(new StringReader("5:hello"));
+            tokenizer = BencodeReader.Create(new StringReader("i-1e"));
+            num = deser.GetBaseObject(tokenizer);
+
+            Assert.IsInstanceOfType(num, typeof(BENumber));
+            Assert.AreEqual(-1, (num as BENumber).ToInt());
+            Assert.AreEqual(-1L, (num as BENumber).ToLong());
+            Assert.AreEqual("-1", num.ToString());
+        }
+
+        /// <summary>
+        /// Verifies GetBaseObject parses strings.
+        /// </summary>
+        [TestMethod]
+        public void IntermediateDeserializer_UnitTest_Strings()
+        {
+            var tokenizer = BencodeReader.Create(new StringReader("5:hello"));
+            var deser = new Deserializer();
             var str = deser.GetBaseObject(tokenizer);
 
-            tokenizer = BencodeReader.Create(new StringReader("d3:key5:valuee"));
+            Assert.IsInstanceOfType(str, typeof(BEByteString));
+            Assert.AreEqual("hello", Encoding.ASCII.GetString((str as BEByteString).Value));
+            Assert.AreEqual("hello", str.ToString());
+        }
+
+        /// <summary>
+        /// TBD
+        /// </summary>
+        [TestMethod]
+        public void IntermediateDeserializer_UnitTest_TBD()
+        {
+            var tokenizer = BencodeReader.Create(new StringReader("d3:key5:valuee"));
+            var deser = new Deserializer();
             var dict = deser.GetBaseObject(tokenizer);
 
             tokenizer = BencodeReader.Create(new StringReader("d1:a1:b1:cd2:ca2:cb2:ccd3:ccadee2:cdl3:cda3:cdb3:cdcee1:d1:ee"));
